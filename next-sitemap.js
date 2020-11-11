88fd909f1a9cf850d0365f9bbe982157f554a5b0
cws <cws@home.fritz.box>
2020-11-11 22:13:44 +0100

    add sitemap

---
+++ next-sitemap.js
@@ -0,0 +1,13 @@
+module.exports = {
+  siteUrl: 'https://next.jamify.org',
+  generateRobotsTxt: true,
+  sitemapSize: 7000,
+  // optional
+  //robotsTxtOptions: {
+  //  additionalSitemaps: [
+  //    'https://next.jamify.org/my-custom-sitemap-1.xml',
+  //    'https://next.jamify.org/my-custom-sitemap-2.xml',
+  //    'https://next.jamify.org/my-custom-sitemap-3.xml',
+  //  ],
+  //},
+}
